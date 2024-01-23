3109440ca37c0ef3129b9e5d01355697fe486968
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2024-01-23 19:39:43 +0700

    Create kernel

--- connection.php
+++ connection.php
@@ -13,8 +13,14 @@ class Connection
         try {
             if (!self::$connection) {
                 $connect_var = new PDO("mysql:host={$config['server']};dbname={$config['dbname']}", $config['dbuser'], $config['dbpass']);
+                $connect_var->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+                $connect_var->setAttribute(\PDO::ATTR_DEFAULT_FETCH_MODE, \PDO::FETCH_ASSOC);
+                self::$connection = $connect_var;
+                return self::$connection;
             }
         } catch (\PDOException $e) {
+            echo $e->getMessage();
+            exit;
         }
     }
 }
