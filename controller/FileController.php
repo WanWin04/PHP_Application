ce38c0994cf6c07e07f48828638b4a3d98397864
WanWin04 <123612410+WanWin04@users.noreply.github.com>
2024-01-24 23:07:19 +0700

    Data item - unfinished

--- controller/FileController.php
+++ controller/FileController.php
@@ -13,6 +13,23 @@ class FileController
 
             if (move_uploaded_file($_FILES["uploadFile"]["tmp_name"], $target_file)) {
                 $message = "The file" . htmlspecialchars(basename($_FILES["uploadFile"]["name"])) . "has been upload";
+
+                // $row = 1;
+                // $columnData = [];
+                // if (($handle = fopen($target_file, "r")) !== FALSE) {
+                //     while (($data == fgetcsv($handle, 11, ",")) !== FALSE) {
+                //         if ($row != 1) {
+                //             $columnData[$row]['name'] = $data[0];
+                //             $columnData[$row]['qt'] = $data[1];
+                //             $columnData[$row]['price'] = $data[2];
+                //         }
+                //         $row++;
+                //     }
+                //     fclose($handle);
+                // }
+                // if (!empty($columnData)) {
+
+                // }
             } else {
                 $message = "ERROR - Try again";
             }
